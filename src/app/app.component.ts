32a61e910d1d1ccd45e63cbe137ee9382610aaaf
letbueno <leticia.bueno.martins@gmail.com>
2020-04-17 16:32:33 -0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Turma } from './turmas';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FormBootstrap';
+  title = 'cadastro';
+  nome = null;
+  data = null;
+  sexo = null;
+  cpf = null;
+  email = null;
+  telefone = null;
+  endereco = null;
+  cep = null;
+  cidade = null;
+  documentos = null;
+  turmas: Array<Turma> = [
+    new Turma(1,'LPWEB'),
+    new Turma(2,'AP2')
+
+
+];
+
+
+  uf = ['AC','AL','AP','AM','BA','DF','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE'
+  ];
 }
+
+
